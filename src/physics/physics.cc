212a96e1b9416e3368224e974489c291f6ebc871
Gustav <sir.gustav.the.coder@gmail.com>
2022-10-11 20:16:53 +0200

    feat: exposed api

--- src/physics/physics.cc
+++ src/physics/physics.cc
@@ -2,92 +2,14 @@
 #include <type_traits>
 
 
-struct Transform
+namespace physics
 {
-	vector3 position;
-	float scale;
-	quaternion rotation;
-};
-
-
-struct Sphere
-{
-	vector3 center;
-	float radius;
-};
-
-struct Plane
-{
-	vector3 normal;
-	float distance;
-};
-
-struct NullShape {};
-
-using CollisionCallback = std::function<void(Collision&, float)>;
-
-using Shape = std::variant<Sphere, Plane, NullShape>;
-
-struct Rigidbody
-{
-	bool is_simulated = true;
-	std::optional<vector3> custom_gravity;
-	vector3 force;    // Net force
-	vector3 velocity;
-
-	float mass = 10.0f;
-	bool takes_gravity = true; // If the rigidbody will take gravity from the world.
-
-	float static_friction = 0.5f;  // Static friction coefficient
-	float dynamic_friction = 0.5f; // Dynamic friction coefficient
-	float restitution = 0.1f;     // Elasticity of collisions (bounciness)
-};
-
-struct CollisionObject
-{
-	Transform transform;
-	Shape shape = NullShape{};
-	bool is_trigger;
-	std::optional<Rigidbody> body;
- 
-	CollisionCallback on_collision;
-};
-
-
-struct CollisionPoints
-{
-	vector3 a; // Furthest point of a into b
-	vector3 b; // Furthest point of b into a
-	vector3 normal; // b – a normalized
-	float depth = 0;    // Length of b – a
-	bool has_collision = false;
-};
 
 CollisionPoints NoCollision()
 {
 	return {};
 }
 
-struct Collision
-{
-    CollisionObject* a;
-    CollisionObject* b;
-    CollisionPoints points;
-};
-
-using Solver = std::function<void (std::vector<Collision>&, float)>;
-
-struct CollisionWorld
-{
-	std::vector<CollisionObject*> objects;
-	std::vector<Solver> solvers;
-	CollisionCallback on_collision;
-};
-
-struct DynamicsWorld : public CollisionWorld
-{
-	vector3 global_gravity = vector3{ 0, -9.81f, 0 };
-};
 
 CollisionPoints TestCollision
 (
@@ -491,3 +413,7 @@ void PositionSolver(std::vector<Collision>& collisions, float dt)
 		bBody->transform.position += resolution * (1.0f - bStatic);
 	}
 }
+
+
+}
+
